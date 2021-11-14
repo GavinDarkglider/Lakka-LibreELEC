aaaa8166b0021313303a220663799d0854ed896f
Ronald Brown <rbrown4014@yahoo.com>
2021-11-14 19:58:30 +0100

    Bootloader cleanups, seperate bootloader stuff for libreelec builds

--- projects/L4T/devices/Switch/packages/cbfstool/package.mk
+++ projects/L4T/devices/Switch/packages/cbfstool/package.mk
@@ -18,52 +18,39 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="switch-coreboot"
+PKG_NAME="cbfstool"
 PKG_VERSION="d0156146"
 PKG_GIT_CLONE_BRANCH="switch-linux"
 PKG_ARCH="any"
-PKG_DEPENDS_HOST="gcc-linaro-aarch64-linux-gnu:host gcc-linaro-arm-linux-gnueabi:host zlib:host openssl:host"
-PKG_DEPENDS_TARGET="toolchain switch-coreboot:host switch-u-boot gcc-linaro-aarch64-linux-gnu:host gcc-linaro-arm-linux-gnueabi:host curl:host"
+PKG_DEPENDS_HOST="zlib:host openssl:host"
 PKG_SITE="https://gitlab.com/switchroot/switch-coreboot"
 PKG_GIT_URL="$PKG_SITE"
 PKG_URL="https://gitlab.com/switchroot/switch-coreboot.git"
-PKG_CLEAN="switch-bootloader"
-PKG_TOOLCHAIN="make"
+PKG_TOOLCHAIN="manual"
 
 PKG_AUTORECONF="no"
 
 make_host() {
-  export PATH=$TOOLCHAIN/lib/gcc-linaro-aarch64-linux-gnu/bin/:$PATH
-  export PATH=$TOOLCHAIN/lib/gcc-linaro-arm-linux-gnueabi/bin/:$PATH
-  export C_INCLUDE_PATH="$TOOLCHAIN/include:$C_INCLUDE_PATH"
-  export LIBRARY_PATH="$TOOLCHAIN/lib:$LIBRARY_PATH"
-  git submodule update --init --recursive
-  make nintendo_switch_defconfig
-  make iasl
-  make tools
+  cd ${PKG_BUILD}/util/cbfstool/
+  make
 }
 
 makeinstall_host() {
-  :
+  if [ -f "${TOOLCHAIN}/bin/cbfstool" ]; then
+    rm -r ${TOOLCHAIN}/bin/cbfstool
+  fi
+  mkdir -p ${INSTALL_PKG}/usr/bin
+  cp -P ${PKG_BUILD}/util/cbfstool/cbfstool ${TOOLCHAIN}/bin
 }
 
 pre_make_host() {
-  sed -i -e "s|../switch-uboot/u-boot.elf|${BUILD}/switch-boot/u-boot.elf|" $PKG_BUILD/configs/nintendo_switch_defconfig
+  :
 }
 
 make_target() {
-  export PATH=$TOOLCHAIN/lib/gcc-linaro-aarch64-linux-gnu/bin/:$PATH
-  export PATH=$TOOLCHAIN/lib/gcc-linaro-arm-linux-gnueabi/bin/:$PATH
-  OLD_CROSS_COMPILE=$CROSS_COMPILE
-  export CROSS_COMPILE=aarch64-linux-gnu-
-
-  # Make
-  make
-
-  export CROSS_COMPILE=$OLD_CROSS_COMPILE
+  :
 }
 
 makeinstall_target() {
-  mkdir -p $BUILD/switch-boot
-  cp $PKG_BUILD/build/coreboot.rom $BUILD/switch-boot
+  :
 }
